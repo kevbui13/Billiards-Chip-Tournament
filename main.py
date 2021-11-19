41cea0a5841e3839866665aa730857e043c38cc8
kevbui13 <94651800+kevbui13@users.noreply.github.com>
2021-11-18 17:04:43 -0800

    Update main.py
    Made some cosmetic changes. Also found an error in the table assignment. (Line 69 and 83).

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ global chips
 # Team Numbers
 teams = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
 
-# Table Location/Descriptions - currently 4 tables
+# Table Location/Descriptions - currently 4 tables, winner and losers side
 pool_table = ['Table 01 L', 'Table 01 W', 'Table 02 L', 'Table 02 W', 'Table 03 L', 'Table 03 W', 'Table 04 L', 'Table 04 W']
 
-# Players at the Tables - 4 tables, 8 positions
+# Players at the Tables - 4 tables, 8 positions, positions 1-8
 active_player_tables = []
 
-# Players available
+# Players waiting pool
 active_players = list.copy(teams)
 
 # Chips team # has. Team 0 has 0 chips.
@@ -61,28 +61,28 @@ if __name__ == '__main__':
                 buffer = active_player_tables[table_num]
                 chips[buffer] = chips[buffer] - 1 # Losing player loses a chip.
 
-                b = random.sample(active_players, 1)
-                while chips[b[0]] < 1:
-                    b = random.sample(active_players, 1)
+                new_player = random.sample(active_players, 1) # Choosing a new player.
+                while chips[new_player[0]] < 1: # Checking that new players has chips.
+                    new_player = random.sample(active_players, 1)
 
-                active_player_tables[table_num] = b[0]
-                print("Send player #", b[0], "to Table:", round((table_num+1)/2))
+                active_player_tables[table_num] = new_player[0]
+                print("Send team #", new_player[0], "to Table Position:", table_num+1)
                 active_players.append(buffer)
-                active_players.remove(b[0])
+                active_players.remove(new_player[0])
 
             elif (table_num % 2) == 1: # If on winner location and lost
                 buffer = active_player_tables[table_num]
                 chips[buffer] = chips[buffer] - 1 # Losing player loses a chip.
                 active_player_tables[table_num] = active_player_tables[table_num - 1]
 
-                b = random.sample(active_players, 1)
-                while chips[b[0]] < 1:
-                    b = random.sample(active_players, 1)
+                new_player = random.sample(active_players, 1) # Choosing a new player.
+                while chips[new_player[0]] < 1: # Checking that new players has chips.
+                    new_player = random.sample(active_players, 1)
 
-                active_player_tables[table_num - 1] = b[0]
-                print("Send player #", b[0], "to Table:", round((table_num+1)/2))
+                active_player_tables[table_num - 1] = new_player[0]
+                print("Send team #", new_player[0], "to Table Position:", table_num+1)
                 active_players.append(buffer)
-                active_players.remove(b[0])
+                active_players.remove(new_player[0])
 
             print_pairings()
             print_available_players()
